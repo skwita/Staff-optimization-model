e0e4c749991d53b90824a655c5a70abb1642140f
skwita <skwita.max@gmail.com>
2024-05-10 12:45:28 +0300

    initial commit

---
+++ src/main/java/com/skwita/staffoptimizationmodel/StaffoptimizationmodelApplication.java
@@ -0,0 +1,13 @@
+package com.skwita.staffoptimizationmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffoptimizationmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffoptimizationmodelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skwita/staffoptimizationmodel/StaffoptimizationmodelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skwita.staffoptimizationmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffoptimizationmodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
